12fdaf3f8e6341be0fc7167febd9807bffbe98f1
EWYan <yanerweiwei@gmail.com>
2022-04-17 16:57:36 +0800

    # print given number of bytes

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,47 @@ use clap::{Arg, Command};
 use std::fs::File;
 use std::io::{self, prelude::*};
 
-const MAX_COUNT:usize = std::usize::MAX;
+const MAX_COUNT: usize = std::usize::MAX;
 
+struct Printer {
+    idx: usize,
+}
+
+impl Printer {
+    fn new() -> Printer {
+        Printer { idx: 1 }
+    }
+    fn print_byte(&mut self, b: u8) -> io::Result<()> {
+        // print column header
+        if self.idx == 1 {
+            print!("offset\\col ");
+            for ci in 0..16 {
+                match ci {
+                    7 => print!("{:02}  ", ci),
+                    _ => print!("{:02} ", ci),
+                }
+            }
+            println!();
+        }
+        // print offset 
+        if self.idx % 16 == 1 {
+            print!("0x{:08x} ", self.idx - 1);
+        }
+        // print byte
+        print!("{:02x} ", b);
+        // control LF
+        match self.idx % 16 {
+            8 => print!(" "),
+            0 => {
+                println!();
+            },
+            _ => {}
+        }
+        // work statistics
+        self.idx += 1;
+        Ok(())
+    }
+}
 fn hexdump() -> io::Result<()> {
     let paras = Command::new(crate_name!())
         .arg(
@@ -28,30 +67,34 @@ fn hexdump() -> io::Result<()> {
     let file_path = paras.value_of("infile").unwrap();
     let bytes_cnt = match paras.value_of("count") {
         Some(cnt) => cnt.parse::<usize>().unwrap(),
-        None => MAX_COUNT
+        None => MAX_COUNT,
     };
-    #[cfg(debug_assertions)] {
+    #[cfg(debug_assertions)]
+    {
         println!("file_path :{}", file_path);
         println!("count :{}", bytes_cnt);
     }
 
     let mut fp = File::open(file_path)?;
     let mut buff = [0_u8; 32];
-    let mut cnt_rec = 0_usize;
-    loop {
+    let mut printer = Printer::new();
+    'max_brk: loop {
         let cnt = fp.read(&mut buff)?;
         if cnt == 0 {
             break;
         }
         for b in &buff[..cnt] {
-            print!("{:02x} ", b);
-        }
-        println!();
-        cnt_rec += cnt;
-        if cnt_rec > bytes_cnt {
-            break;
+            let res = printer.print_byte(*b);
+            match res {
+                Ok(_) => {}, 
+                Err(_) => break,
+            }
+            if printer.idx > bytes_cnt {
+                break 'max_brk;
+            }
         }
     }
+    println!("\nTotal Bytes: 0x{:08X}({})", printer.idx - 1, printer.idx - 1);
     Ok(())
 }
 
